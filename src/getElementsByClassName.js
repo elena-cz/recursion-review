43b273cb8d7c6824d3cd79cd2f956d3bb8d09f4c
elena-cz <elenaczubiak@gmail.com>
2017-10-09 12:27:49 -0700

    Implement getElementsbyClass and fix test

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var results = [];
+
+  var startNode = document.body;
+  
+  var findNodes = function(node) {
+    var classes = node.classList;
+    
+    if (classes && classes.contains(className)) {
+      results.push(node);
+    }
+    
+    if (node.hasChildNodes()) {
+      var children = node.childNodes;
+      children.forEach(function(el) {
+        findNodes(el);
+      });
+    }
+  };
+
+  findNodes(startNode);
+
+  return results;
 };
